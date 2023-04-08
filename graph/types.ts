c265ca0ec5b54c055f2f46714c3e672b0e265d30
Erik Strand <erik.strand@cba.mit.edu>
2023-04-08 14:08:13 -0400

    Explain the asymmetry in input/output types

--- graph/types.ts
+++ graph/types.ts
@@ -1,8 +1,12 @@
 type GLangNode<NodeData> = {
     data: NodeData;
 
-    // these strings are GLangEdge ids
+    // Each input can only have zero or one edges connected to it.
+    // If present, the string is a GLangEdge id.
     inputs: Array<string | null>
+
+    // Each output can be connected to multiple edges.
+    // The strings are GLangEdge ids.
     outputs: Array<Set<string>>
 };
 
